df8f0ed4cc52d1aa9249e374d5322184a7bbf9ce
Rémy MEJA <remy.meja@inist.fr>
2017-05-03 13:27:24 +0200

    fix temporary test file

--- src/octachore.js
+++ src/octachore.js
@@ -1,5 +1,5 @@
 const octachore = require(__dirname+'/../build/Release/octachore.node');
 // const octachore = require(__dirname+'/../build/Debug/octachore.node');
-octachore.getAllComponentImage("/home/meja/Dev/envrac/tiff/lorem-ipsum.tif", 5, (code) => {
+octachore.getAllComponentImage("/home/meja/Dev/envrac/tiff/lorem-ipsum.tif", 2, (code) => {
   console.log(code);
 })
