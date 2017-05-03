12f2fdf9d1ac6ad430b31a13e5e84e458af9d8af
Rémy MEJA <remy.meja@inist.fr>
2017-05-03 15:58:26 +0200

    delete unwanted test

--- src/octachore.js
+++ src/octachore.js
@@ -1,9 +1,2 @@
 const octachore = require("bindings")("octachore");
-octachore.getAllComponentImage("/home/meja/Dev/envrac/tiff/lorem-ipsum.tif", 2, (error, results) => {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log(results)
-  }
-})
 module.exports = octachore;
